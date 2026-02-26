8e0a80dfe1300ea95dbe6d749f7fcc1699d0282a
Clint Bobo <clintbobo54@gmail.com>
2026-02-27 04:36:37 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,16 +3,13 @@ import cors from "cors";
 
 const app = express();
 
-/* Middlewares */
 app.use(cors());
 app.use(express.json());
 
-/* Root Test */
 app.get("/", (req, res) => {
   res.send("🚀 Bobo Analytics Backend Running Successfully");
 });
 
-/* HEALTH API (THIS WAS MISSING) */
 app.get("/api/health", (req, res) => {
   res.json({
     status: "OK",
