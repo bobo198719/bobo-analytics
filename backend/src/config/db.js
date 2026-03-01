ef85553a2d854d7eeadc26e227f016178fab3148
Clint Bobo <clintbobo54@gmail.com>
2026-03-01 22:58:45 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,20 +1,16 @@
 import { MongoClient } from "mongodb";
 
 const MONGO_URI =
-"mongodb+srv://clintbobo54_db_user:Bobo@12345@cluster0.jegbytg.mongodb.net/?retryWrites=true&w=majority";
-
-let client;
+"mongodb+srv://clintbobo54_db_user:Bobo%4012345@cluster0.jegbytg.mongodb.net/?retryWrites=true&w=majority";
 
 export const connectDB = async () => {
   try {
-    client = new MongoClient(MONGO_URI);
-
+    const client = new MongoClient(MONGO_URI);
     await client.connect();
 
     console.log("✅ MongoDB Connected Successfully");
   } catch (error) {
-    console.error("❌ MongoDB Connection Failed");
-    console.error(error);
+    console.error("❌ MongoDB Connection Failed:", error);
     process.exit(1);
   }
 };
