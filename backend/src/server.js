ae777f7ecce496c02d153406bea34db7a9335adc
Clint Bobo <clintbobo54@gmail.com>
2026-03-02 09:38:55 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,23 +4,18 @@ import { connectDB } from "./config/db.js";
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Test Route
 app.get("/", (req, res) => {
   res.send("Bobo Analytics API Running 🚀");
 });
 
-// Start Server ONLY after DB connects
 const startServer = async () => {
   await connectDB();
 
-  const PORT = 5000;
-
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
+  app.listen(5000, () => {
+    console.log("🚀 Server running on port 5000");
   });
 };
 
