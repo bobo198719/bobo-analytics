2e5ba4a560fb9682dadd8b36a9ef071f9aa0ae20
Clint Bobo <clintbobo54@gmail.com>
2026-03-02 09:56:12 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,19 +3,34 @@ import cors from "cors";
 import { connectDB } from "./config/db.js";
 
 const app = express();
+const PORT = 5000;
 
+/* ======================
+   MIDDLEWARE
+====================== */
 app.use(cors());
 app.use(express.json());
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("Bobo Analytics API Running 🚀");
+/* ======================
+   HEALTH CHECK API
+====================== */
+app.get("/api/health", (req, res) => {
+  res.json({
+    status: "OK",
+    message: "Bobo Analytics API Working 🚀",
+  });
 });
 
+/* ======================
+   START SERVER
+====================== */
+
 const startServer = async () => {
+  // CONNECT DATABASE FIRST
   await connectDB();
 
-  app.listen(5000, () => {
+  // THEN START SERVER
+  app.listen(PORT, () => {
     console.log("🚀 Server running on port 5000");
   });
 };
