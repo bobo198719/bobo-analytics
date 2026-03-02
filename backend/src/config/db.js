19d7a375e5e5d97ca0e122864b8242af5ada3e4a
Clint Bobo <clintbobo54@gmail.com>
2026-03-02 09:27:51 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,14 +3,20 @@ import { MongoClient } from "mongodb";
 const MONGO_URI =
 "mongodb+srv://clintbobo54_db_user:Bobo%4012345@cluster0.jegbytg.mongodb.net/?retryWrites=true&w=majority";
 
+let client;
+
 export const connectDB = async () => {
   try {
-    const client = new MongoClient(MONGO_URI);
+    console.log("⏳ Connecting to MongoDB...");
+
+    client = new MongoClient(MONGO_URI);
+
     await client.connect();
 
     console.log("✅ MongoDB Connected Successfully");
+
   } catch (error) {
-    console.error("❌ MongoDB Connection Failed:", error);
+    console.error("❌ MongoDB Connection Failed:", error.message);
     process.exit(1);
   }
 };
