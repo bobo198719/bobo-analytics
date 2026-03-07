f26adb182854de92574b53ce9fbb7380b60db8a5
Clint Bobo <clintbobo54@gmail.com>
2026-03-07 14:09:42 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ const cors=require("cors");
 
 const authRoutes=require("./routes/authRoutes");
 const inventoryRoutes=require("./routes/inventoryRoutes");
+const billRoutes=require("./routes/billRoutes");
 
 const app=express();
 
@@ -11,5 +12,6 @@ app.use(express.json());
 
 app.use("/api/users",authRoutes);
 app.use("/api/inventory",inventoryRoutes);
+app.use("/api",billRoutes);
 
 module.exports=app;
