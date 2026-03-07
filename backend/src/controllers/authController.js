5efdea2006b3e763206ba89b17ffa9be6b3baded
Clint Bobo <clintbobo54@gmail.com>
2026-03-07 13:47:18 +0530

    Update authController.js

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,27 +1,55 @@
-const fs = require("fs");
-const path = require("path");
+const fs=require("fs");
+const path=require("path");
 
-const dbPath = path.join(__dirname, "../database/db.json");
+const dbPath=path.join(__dirname,"../database/db.json");
 
-exports.loginUser = (req, res) => {
+exports.loginUser=(req,res)=>{
 
-const { pharmacyId, password } = req.body;
+const {pharmacyId,password}=req.body;
 
-const db = JSON.parse(fs.readFileSync(dbPath));
+const db=JSON.parse(fs.readFileSync(dbPath));
 
-const user = db.pharmacies.find(
-p => p.pharmacyId === pharmacyId && p.password === password
+const user=db.pharmacies.find(
+p=>p.pharmacyId===pharmacyId && p.password===password
 );
 
 if(!user){
+
 return res.status(401).json({
-error: "Invalid login"
+error:"Invalid login"
 });
+
 }
 
 res.json({
-message: "Login successful",
-pharmacy: user.pharmacy
+message:"Login successful",
+pharmacy:user.pharmacy
+});
+
+};
+
+exports.createPharmacy=(req,res)=>{
+
+const {pharmacy,pharmacyId,password}=req.body;
+
+const db=JSON.parse(fs.readFileSync(dbPath));
+
+const newPharmacy={
+id:Date.now(),
+pharmacy,
+pharmacyId,
+password,
+plan:"999",
+status:"active"
+};
+
+db.pharmacies.push(newPharmacy);
+
+fs.writeFileSync(dbPath,JSON.stringify(db,null,2));
+
+res.json({
+message:"Pharmacy created",
+pharmacy:newPharmacy
 });
 
 };
