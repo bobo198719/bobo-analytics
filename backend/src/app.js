811c5004ffeb761bee5c474c3446bc4c0733fdd7
Clint Bobo <clintbobo54@gmail.com>
2026-03-07 14:00:38 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,36 +1,15 @@
-const express = require("express");
-const cors = require("cors");
+const express=require("express");
+const cors=require("cors");
 
-const authRoutes = require("./routes/authRoutes");
-const inventoryRoutes = require("./routes/inventoryRoutes");
+const authRoutes=require("./routes/authRoutes");
+const inventoryRoutes=require("./routes/inventoryRoutes");
 
-const app = express();
-
-/* Middlewares */
+const app=express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-/* API Routes */
+app.use("/api/users",authRoutes);
+app.use("/api/inventory",inventoryRoutes);
 
-app.use("/api/users", authRoutes);
-app.use("/api/inventory", inventoryRoutes);
-
-/* Health Check */
-
-app.get("/", (req, res) => {
-  res.json({
-    message: "Bobo Analytics Backend Running"
-  });
-});
-
-/* 404 Handler */
-
-app.use((req, res) => {
-  res.status(404).json({
-    error: "Route not found"
-  });
-});
-
-module.exports = app;
+module.exports=app;
