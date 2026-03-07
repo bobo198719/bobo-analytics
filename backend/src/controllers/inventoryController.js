4e9888a5f03df868f692d6c1da3ab48cac89785a
Clint Bobo <clintbobo54@gmail.com>
2026-03-07 13:58:46 +0530

    Update inventoryController.js

--- backend/src/controllers/inventoryController.js
+++ backend/src/controllers/inventoryController.js
@@ -1,42 +1,42 @@
-const fs = require("fs");
-const path = require("path");
+const fs=require("fs");
+const path=require("path");
 
-const dbPath = path.join(__dirname, "../database/db.json");
+const dbPath=path.join(__dirname,"../database/db.json");
 
-exports.getInventory = (req, res) => {
+exports.getInventory=(req,res)=>{
 
-const pharmacyId = req.query.pharmacyId;
+const {pharmacyId}=req.params;
 
-const db = JSON.parse(fs.readFileSync(dbPath));
+const db=JSON.parse(fs.readFileSync(dbPath));
 
-const inventory = db.inventory.filter(
-item => item.pharmacyId === pharmacyId
+const inventory=db.inventory.filter(
+i=>i.pharmacyId===pharmacyId
 );
 
 res.json(inventory);
 
 };
 
-exports.addMedicine = (req, res) => {
+exports.addMedicine=(req,res)=>{
 
-const { pharmacyId, medicine, stock, expiry } = req.body;
+const {pharmacyId,medicine,stock,expiry}=req.body;
 
-const db = JSON.parse(fs.readFileSync(dbPath));
+const db=JSON.parse(fs.readFileSync(dbPath));
 
-const newMedicine = {
+const newItem={
 pharmacyId,
 medicine,
 stock,
 expiry
 };
 
-db.inventory.push(newMedicine);
+db.inventory.push(newItem);
 
-fs.writeFileSync(dbPath, JSON.stringify(db, null, 2));
+fs.writeFileSync(dbPath,JSON.stringify(db,null,2));
 
 res.json({
-message: "Medicine added successfully",
-medicine: newMedicine
+message:"Medicine added",
+item:newItem
 });
 
 };
